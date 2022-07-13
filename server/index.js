2ceb0d4ddad7493f0566097221afebd0997f379e
michealgarcia <garciamicheal0@outlook.com>
2022-07-12 21:57:15 -0700

    connected front end to back end using Axios and cors libraries, then used promises and hooks to grab input data from user and save it to backend. Then displayed the data onto the frontend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
+
 
 const WorkoutModel = require("./models/Workouts");
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
     'mongodb+srv://admin:Garguell1!@crud.mridttg.mongodb.net/workouts?retryWrites=true&w=majority',
@@ -13,15 +16,38 @@ mongoose.connect(
     }
 );
 
-app.get("/", async (req, res) => {
-    const workouts = new WorkoutModel({ workoutDate: "July 7, 2022", workoutName: "push ups", workoutCount: 0 });
+// to insert data into database
+app.post("/insert", async (req, res) => {
+
+    const workoutDate = req.body.workoutDate;
+    const workoutName = req.body.workoutName;
+    const workoutCount = req.body.workoutCount;
+
+    const workouts = new WorkoutModel({
+        workoutDate: workoutDate,
+        workoutName: workoutName,
+        workoutCount: workoutCount
+    });
 
     try {
         await workouts.save();
+        res.send("inserted data");
     } catch (err) {
         console.log(err);
     }
-})
+});
+
+// to display data/ connect to app.js with useEffect
+app.get("/read", async (req, res) => {
+    // to find specific data use find({$where: {item: "specific"}})
+    WorkoutModel.find({}, (err, result) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.send(result);
+    })
+});
 
 
 app.listen(3001, () => {
